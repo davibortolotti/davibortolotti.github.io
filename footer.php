440dbecf13ed8090acc0a0095582cb377e262f1c
Bortolotti <davibortolotti@gmail.com>
2018-04-15 13:45:06 -0300

    artesurpresa

---
+++ footer.php
@@ -0,0 +1,19 @@
+
+
+<footer class="blog-footer">
+ <div>
+   <h3>Quem recomenda a arte surpresa.</h3>
+   <hr />
+   <h4>Formas de Pagamento</h4> <h4>Segurança</h4>
+ </div>
+</footer>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
